59c4b2cce390ac67a10110a623dd70035f9b09e9
PeyloW <peylow@gmail.com>
2014-06-27 21:09:37 +0200

    Added bench mark test project

--- include/time.h
+++ include/time.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+#define CLOCKS_PER_SEC 200
+
 typedef long time_t;
 typedef long clock_t;
 typedef long suseconds_t;
--- sources/time.c
+++ sources/time.c
@@ -29,3 +29,16 @@ int gettimeofday(struct timeval *tp, struct timezone *tzp)
 	return 0;
 }
 
+time_t time(time_t *tloc)
+{
+	struct timeval now;
+
+	if (gettimeofday (&now, NULL) != 0)
+		return ((time_t) -1);
+
+	if (tloc)
+		*tloc = now.tv_sec;
+
+	return now.tv_sec;
+}
+
